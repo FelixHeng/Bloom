a0ae8f90348fd6eb55f7d28c5e90e24c1c2f838d
Felix Heng <felixheng@MacBook-Pro-de-Felix.local>
2020-11-12 09:07:46 +0100

    create user table in the db

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core"
 import path from "path";
+import { User } from './entities/User';
 
 export default {
     migrations: {
         path: path.join(__dirname, "./migrations"),
         pattern: /^[\w-]+\d+\.[tj]s$/,
     },
-    entities: [Post],
+    entities: [Post, User],
     dbName: 'bloom',
     type: 'postgresql',
     debug: !__prod__
