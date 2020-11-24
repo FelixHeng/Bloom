596cb0e4e57a06dc6953e86eda9e199006af8e64
Felix Heng <felixheng@MacBook-Pro-de-Felix.local>
2020-11-24 01:03:41 +0100

    add vote status

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -103,51 +103,57 @@ export class PostResolver {
   @Query(() => PaginatedPosts)
   async posts(
     @Arg("limit", () => Int) limit: number,
-    @Arg("cursor", () => String, { nullable: true }) cursor: string | null
+    @Arg("cursor", () => String, { nullable: true }) cursor: string | null,
+    @Ctx() { req }: MyContext
   ): Promise<PaginatedPosts> {
     // 20 -> 21
     const realLimit = Math.min(50, limit);
     const realLimitPlusOne = realLimit + 1;
 
-    // const replacements: any[] = [realLimitPlusOne];
-
-    // if (cursor) {
-    //   replacements.push(new Date(parseInt(cursor)));
-    // }
-
-    // const postsFetched = await getConnection().query(
-    //   `
-    // select p.*,
-    // json_build_object(
-    //   'id', u.id,
-    //   'username', u.username,
-    //   'email', u.email,
-    //   'createdAt', u."createdAt",
-    //   'updatedAt', u."updatedAt"
-    //   ) creator
-    // from post p
-    // inner join public.user u on u.id = p."creatorId"
-    // ${cursor ? `where p."createdAt" < $2` : ""}
-    // order by p."createdAt" DESC
-    // limit $1
-    // `,
-    //   replacements
-    // );
-
-    const qb = getConnection()
-      .getRepository(Post)
-      .createQueryBuilder("p")
-      .innerJoinAndSelect("p.creator", "creator")
-      .orderBy("p.createdAt", "DESC")
-      .take(realLimitPlusOne);
+    const replacements: any[] = [realLimitPlusOne, req.session.userId];
 
     if (cursor) {
-      qb.where('p."createdAt" < :cursor', {
-        cursor: new Date(parseInt(cursor)),
-      });
+      replacements.push(new Date(parseInt(cursor)));
     }
 
-    const postsFetched = await qb.getMany();
+    const postsFetched = await getConnection().query(
+      `
+      select p.*,
+      json_build_object(
+        'id', u.id,
+        'username', u.username,
+        'email', u.email,
+        'createdAt', u."createdAt",
+        'updatedAt', u."updatedAt"
+        ) creator,
+      ${
+        req.session.userId
+          ? '(select value from updoot where "userId" = $2 and "postId" = p.id) "voteStatus"'
+          : 'null as "voteStatus"'
+      }
+      from post p
+      inner join public.user u on u.id = p."creatorId"
+      ${cursor ? `where p."createdAt" < $3` : ""}
+      order by p."createdAt" DESC
+      limit $1
+      `,
+      replacements
+    );
+
+    // const qb = getConnection()
+    //   .getRepository(Post)
+    //   .createQueryBuilder("p")
+    //   .innerJoinAndSelect("p.creator", "creator")
+    //   .orderBy("p.createdAt", "DESC")
+    //   .take(realLimitPlusOne);
+
+    // if (cursor) {
+    //   qb.where('p."createdAt" < :cursor', {
+    //     cursor: new Date(parseInt(cursor)),
+    //   });
+    // }
+
+    // const postsFetched = await qb.getMany();
     return {
       posts: postsFetched.slice(0, realLimit),
       hasMore: postsFetched.length === realLimitPlusOne,
